a63f7fae74dcb72dd5e5941ce53bdd6087fc30c8
kzndotsh <admin@kaizen.wtf>
2024-09-15 04:35:59 -0400

    feat(ui): Add Values component for displaying company values and principles

---
+++ components/blocks/values.tsx
@@ -0,0 +1,47 @@
+export const Values = () => {
+  return (
+    <section className='py-32'>
+      <div className='container'>
+        <div className='grid gap-8 lg:grid-cols-3'>
+          <h2 className='row-span-2 text-3xl font-semibold lg:text-5xl'>
+            Our Values and Principles
+          </h2>
+          <div>
+            <h3 className='mb-2 text-xl font-medium'>Team Spirit</h3>
+            <p className='text-muted-foreground'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+              architecto atque consequuntur perferendis ratione dolorem vitae,
+              doloribus facere.
+            </p>
+          </div>
+          <div>
+            <h3 className='mb-2 text-xl font-medium'>Innovation</h3>
+            <p className='text-muted-foreground'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+              architecto atque consequuntur perferendis ratione dolorem vitae,
+              doloribus facere.
+            </p>
+          </div>
+          <div>
+            <h3 className='mb-2 text-xl font-medium'>Quality</h3>
+            <p className='text-muted-foreground'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+              architecto atque consequuntur perferendis ratione dolorem vitae,
+              doloribus facere.
+            </p>
+          </div>
+          <div>
+            <h3 className='mb-2 text-xl font-medium'>Integrity</h3>
+            <p className='text-muted-foreground'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+              architecto atque consequuntur perferendis ratione dolorem vitae,
+              doloribus facere.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Values;
