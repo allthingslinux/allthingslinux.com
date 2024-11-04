3b5b9fbfc8576daffcc6431121f2544b999435ca
kzndotsh <admin@kaizen.wtf>
2024-11-04 10:25:18 -0500

    refactor(page.tsx): remove Navbar and DiscordCta components to simplify layout feat(page.tsx): add Testimonials component to enhance user engagement style(page.tsx): update className quotes for consistency chore(page.tsx): comment out Values component for potential future use

--- app/page.tsx
+++ app/page.tsx
@@ -2,34 +2,25 @@ import Image from 'next/image';
 
 import Hero from '@/components/blocks/hero';
 import Values from '@/components/blocks/values';
-import Navbar from '@/components/blocks/navbar';
 import Stats from '@/components/blocks/stats';
 import Cards from '@/components/blocks/cards';
-import DiscordCta from '@/components/blocks/discord-cta';
 import DonateCta from '@/components/blocks/donate-cta';
+import Testimonials from '@/components/blocks/testimonials';
 
 export default function Home() {
   return (
-    <div className='min-h-screen flex flex-col'>
-      <header className=''>
-        <div className='container mx-auto px-4 py-4'>
-          <nav>
-            <Navbar />
-          </nav>
-        </div>
-      </header>
-
-      <main className='flex-grow container mx-auto px-4 py-8 flex flex-col gap-8'>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow mx-auto px-4 py-8 flex flex-col gap-8 container">
         <Hero />
-        <DiscordCta />
+        <Testimonials />
         <Cards />
-        <Values />
+        {/* <Values /> */}
         <Stats />
         <DonateCta />
       </main>
 
-      <footer className='bg-[#1a1b26] text-white py-4'>
-        <div className='container mx-auto text-center'>
+      <footer className="bg-[#1a1b26] text-white py-4">
+        <div className="container mx-auto text-center">
           {new Date().getFullYear()} | All Things Linux
         </div>
       </footer>
