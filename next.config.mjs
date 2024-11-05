cdf6600ed88cc3bb87e096dc10de46fbb4dcc351
kzndotsh <admin@kaizen.wtf>
2024-11-05 13:15:50 -0500

    docs(code-of-conduct): add initial Code of Conduct page to establish community guidelines
    chore(next.config.mjs): enable reactStrictMode and disable poweredByHeader for improved performance and security
    
    chore(next.config.mjs): remove unused remark plugins to simplify configuration
    
    build(package.json): update dependencies and devDependencies to latest versions for improved stability and features

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 import createMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
-import remarkGitContributors from 'remark-git-contributors';
+// import remarkGfm from 'remark-gfm';
+// import remarkGitContributors from 'remark-git-contributors';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
 const withMDX = createMDX({
+  extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [remarkGfm, remarkGitContributors],
+    remarkPlugins: [],
     rehypePlugins: [],
   },
 });
